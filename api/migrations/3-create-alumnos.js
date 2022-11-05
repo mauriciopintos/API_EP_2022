0067176c147c9e3ff5a6d5d385491210159d0bc7
EnzoAlarcon <enzoalarcon01@gmail.com>
2022-11-05 12:58:41 -0300

    auto increment true en el id de alumnos

--- api/migrations/3-create-alumnos.js
+++ api/migrations/3-create-alumnos.js
@@ -4,7 +4,7 @@ module.exports = {
     return queryInterface.createTable('alumnos', {
       id: {
         allowNull: false,
-        autoIncrement: false,
+        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
