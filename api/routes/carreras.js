19595bae816e1097bf151620ca0b2d1b9507ca29
EnzoAlarcon <enzoalarcon01@gmail.com>
2022-11-16 00:10:40 -0300

    Se agrega validacion de token al route de departamentos y carreras, más consultas de departamentos y carreras en cliente.http

--- api/routes/carreras.js
+++ api/routes/carreras.js
@@ -4,12 +4,14 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 const ControladorCarreras = require('../controladores/Carreras');
+const ControladorLogin = require('../controladores/logins');
+const validaToken = ControladorLogin.validaToken
 
-router.get('/', ControladorCarreras.get);
-router.get('/pag', ControladorCarreras.getPaginado); //ARREGLAR
-router.post('/alta', ControladorCarreras.post);
-router.get('/:cod_carrera', ControladorCarreras.getConCodigo );
-router.put('/modificacion/:cod_carrera', ControladorCarreras.putConCodigo);
-router.delete('/baja/:cod_carrera', ControladorCarreras.deleteConCodigo);
+router.get('/:user', validaToken, ControladorCarreras.get);
+router.get('/:user/pag', validaToken, ControladorCarreras.getPaginado);
+router.post('/:user/alta', validaToken, ControladorCarreras.post);
+router.get('/:user/:cod_carrera', validaToken, ControladorCarreras.getConCodigo );
+router.put('/:user/modificacion/:cod_carrera', validaToken, ControladorCarreras.putConCodigo);
+router.delete('/:user/baja/:cod_carrera', validaToken, ControladorCarreras.deleteConCodigo);
 
 module.exports = router;
--- api/routes/departamentos.js
+++ api/routes/departamentos.js
@@ -4,12 +4,14 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 const ControladorDepartamentos = require('../controladores/Departamentos');
+const ControladorLogin = require('../controladores/logins');
+const validaToken = ControladorLogin.validaToken;
 
-router.get('/', ControladorDepartamentos.get); 
-router.get('/pag', ControladorDepartamentos.getPaginado); //ARREGLAR
-router.post('/alta', ControladorDepartamentos.post); 
-router.get('/:cod_departamento', ControladorDepartamentos.getConCodigo ); 
-router.put('/modificacion/:cod_departamento', ControladorDepartamentos.putConCodigo);
-router.delete('/baja/:cod_departamento', ControladorDepartamentos.deleteConCodigo);
+router.get('/:user', validaToken, ControladorDepartamentos.get); 
+router.get('/:user/pag', validaToken, ControladorDepartamentos.getPaginado);
+router.post('/:user/alta', validaToken, ControladorDepartamentos.post); 
+router.get('/:user/:cod_departamento', validaToken, ControladorDepartamentos.getConCodigo ); 
+router.put('/:user/modificacion/:cod_departamento', validaToken, ControladorDepartamentos.putConCodigo);
+router.delete('/:user/baja/:cod_departamento', validaToken, ControladorDepartamentos.deleteConCodigo);
 
 module.exports = router;
